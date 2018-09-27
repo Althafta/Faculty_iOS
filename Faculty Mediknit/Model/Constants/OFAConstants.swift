c5e5554d312927eb7d9f0821e57c35b2db8a8637
Syam PJ <user@syams-imac.local>
2018-09-27 11:38:20 +0530

    Project Initialisation

---
+++ Faculty Mediknit/Model/Constants/OFAConstants.swift
@@ -0,0 +1,86 @@
+//
+//  Constants.swift
+//  CompassForSuccess
+//
+//  Created by Bon User on 9/5/16.
+//  Copyright © 2016 Bon User. All rights reserved.
+//
+
+import Foundation
+
+//APIDetails :- http://beta.ofabee.com/assets/apis/
+
+var DomainKey = "Domain_Key"
+
+let barTintColor = "4463b3"//primary
+
+let device_token="DEVICE_TOKEN"
+
+let ArrayRecentSearches="Recent_Searches"
+
+let orderAcceptColorCode = "0ca500"
+
+let buttonBackgroundColor = "A9B8DE"
+
+let backgroundColor = "E3E9ED"
+
+let cellBackground = "f2f6ff"
+
+let materialRedColor = "FF4F49"
+
+let USERNAME="USERNAME"
+
+let ACCESS_TOKEN = "access_token"
+
+let InstabugID = "10d7fb568e769d3f2bb96f50aba5abf3" // Beta
+
+//let InstabugID = "322448d4c58871875ff73f7bdf809f8c" // Live
+
+let GoogleClientID = "994191700394-vpmbhp8qi6cla457nnij7j288pdbvg9m.apps.googleusercontent.com"
+
+let USER_ID = "USER_ID"
+
+let EMAIL = "EMAIL"
+
+var ARRAY_CASE_PHOTOS = NSMutableArray()
+
+var ARRAY_CASE_PRIORITY = NSMutableArray()
+
+var ARRAY_CASE_ROLE = NSMutableArray()
+
+var ARRAY_CASE_SPECIALITY = NSMutableArray()
+
+var ARRAY_CASE_APPROACH = NSMutableArray()
+
+var ARRAY_CASE_FORM_DATA = NSMutableArray()
+
+var currentPage = "CurrentPageIndex"
+
+var arrayFormAddCase = "ARRAY_FORM_ADD_CASE"
+
+var original_arrayFormAddCase = "Original_ARRAY_FORM_ADD_CASE"
+
+var isSyncEnabled = "isSyncEnabled"
+
+var isConcentGrantedUD = "isConcentGranted"
+
+var BOOK_NAME = "Book_Name"
+
+var CASE_ID = ""
+
+var currencySymbol = "CURRENCY"
+
+let sectionBackgroundColor = "EFEFF4"
+
+//local
+
+let FormURLString = "https://surgeon-api.mediknit.org/json/lap.json"
+
+//let FormURLString = "https://surgeon-api.mediknit.org/json/acl.json"
+
+let userBaseURL = "https://surgeon-apiv2.mediknit.org/"
+//test
+//let userBaseURL = "http://test.onlineprofesor.com/index.php/"
+//live
+//let userBaseURL = "https://lifeline.ofabee.com/"
+
